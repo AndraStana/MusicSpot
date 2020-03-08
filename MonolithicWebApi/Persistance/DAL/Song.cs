ae5d7955e132eb04054112b0fc055fe9fcecbc4c
AndraStana <andra.stana@yahoo.com>
2020-03-08 15:30:07 +0200

    Initial commit

---
+++ MonolithicWebApi/Persistance/DAL/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.DAL
+{
+    class Song
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ MonolithicWebApi/Persistance/Infrastructure/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Infrastructure
+{
+    class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\SQLEXPRESS;Database=MusicSpot;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+    }
+}
